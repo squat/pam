25c49c5ea23a28ad22424ef84652a4e7726545b3
squat <lserven@gmail.com>
2025-06-06 14:46:31 +0200

    nix: migrate to flake-parts and install pamtester
    This commit migrates the flake configuration to use flake-parts for the
    purpose of allowing easy overlaying of nixpkgs so that we can install
    pamtester on Darwin. This overlay will be necessary until
    https://github.com/NixOS/nixpkgs/pull/414364 is merged.
    
    Signed-off-by: squat <lserven@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,64 +3,67 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    pre-commit-hooks = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks-nix = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      pre-commit-hooks,
-      ...
-    }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.git-hooks-nix.flakeModule
+      ];
+      systems = [
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
       ];
-    in
-    {
-      checks = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
+      perSystem =
         {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              actionlint.enable = true;
-              nixfmt-rfc-style.enable = true;
-              gofmt.enable = true;
-              govet.enable = true;
-              govet.extraPackages = [ pkgs.pam ];
+          pkgs,
+          system,
+          config,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                pamtester = prev.pamtester.overrideAttrs (_: _: { meta.platform = prev.lib.platforms.unix; });
+              })
+            ];
+            config = { };
+          };
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              src = ./.;
+              hooks = {
+                actionlint.enable = true;
+                nixfmt-rfc-style.enable = true;
+                gofmt.enable = true;
+                govet.enable = true;
+                govet.extraPackages = [ pkgs.pam ];
+              };
             };
           };
-        }
-      );
-
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            inherit (self.checks.${system}.pre-commit-check) shellHook;
-            packages =
-              with pkgs;
-              [
-                go
-                pam
-                pamtester
-              ]
-              ++ self.checks.${system}.pre-commit-check.enabledPackages;
+          devShells = {
+            default = pkgs.mkShell {
+              inherit (config.pre-commit.devShell) shellHook;
+              packages =
+                with pkgs;
+                [
+                  go
+                  pam
+                  pamtester
+                ]
+                ++ config.pre-commit.settings.enabledPackages;
+            };
           };
-        }
-      );
+        };
     };
 }
